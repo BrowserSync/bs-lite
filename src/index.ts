7c145da083e34979233900b6821f29179b257e75
Shane Osbourne <shane.osbourne8@gmail.com>
2017-07-14 17:29:51 +0100

    Create MW

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,11 @@ import server, {IServerOptions, Middleware} from './plugins/server';
 
 const {createSystem} = actorJS;
 
+export interface BSCommonOptions {
+    cwd: string;
+    string: boolean;
+}
+
 const pluginWhitelist = {
     'serveStatic': serveStatic,
     'server': server
@@ -22,29 +27,46 @@ export default function init(options: object) {
         if (options[name]) {
             return {
                 name,
-                options: options[name],
+                input: options[name],
                 factory: pluginWhitelist[name],
             }
         }
         return null;
     }).filter(Boolean);
 
+    const commonOptions = {
+        cwd: process.cwd()
+    }
+    
+    function createPayload(input = {}) {
+        return {
+            options: commonOptions,
+            input
+        }
+    }
+
     Observable
         .from(setupActors)
         .flatMap(x => {
             return system
                 .actorOf(x.factory, x.name)
-                .ask({type: 'init', payload: x.options});
+                .ask({
+                    type: 'init',
+                    payload: createPayload(x.input)
+                });
         })
         .pluck('mw')
         .reduce((acc: Middleware[], item: Middleware) => acc.concat(item), [])
         .flatMap(middleware => {
+
+            const opts: IServerOptions = {
+                middleware: middleware,
+                ...options['server']
+            };
+
             return httpServer.ask({
                 type: 'init',
-                payload: <IServerOptions>{
-                    middleware: middleware,
-                    ...options['server']
-                }
+                payload: opts
             });
         })
         // .flatMap(actor => actor.ask({type: 'init'}))
--- src/plugins/serveStatic.ts
+++ src/plugins/serveStatic.ts
@@ -1,18 +1,34 @@
 import {Observable} from 'rxjs';
 import {IActorContext} from "aktor-js/dist/ActorContext";
+import {BSCommonOptions} from "../index";
+
 import {Actor} from "aktor-js/dist/createActor";
 import {ActorRef} from "aktor-js/dist/ActorRef";
+import {join, parse} from "path";
+
+interface SSIncoming {
+    input: string|string[]
+    options: BSCommonOptions
+}
 
 export default function(address: string, context: IActorContext) {
 
-    function createMiddleware(options) {
-        return options.map(dir => {
-            return {
-                route: '',
-                dir,
-                handle: require('serve-static')(dir)
-            }
-        })
+    function createMiddleware(incoming: SSIncoming) {
+        const input = [].concat(incoming.input);
+        return input
+            .map(input => {
+                return {
+                    input,
+                    parsed: parse(input),
+                    resolved: join(incoming.options.cwd, input)
+                }
+            })
+            .map(item => {
+                return {
+                    route: '',
+                    handle: require('serve-static')(item.resolved)
+                }
+            });
     }
 
     return {
