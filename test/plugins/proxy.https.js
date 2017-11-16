b4b922e306402f698eaceee85ac340d6c0c8a40d
Shane Osbourne <shane.osbourne8@gmail.com>
2017-11-16 19:17:36 +0000

    Add tests for https proxies

--- test/plugins/proxy.https.js
+++ test/plugins/proxy.https.js
@@ -1,44 +1,91 @@
 require('source-map-support').install();
-const {DefaultOptions} = require('../../dist/options');
-const {createSystem, fromOptions} = require('../../dist');
-const {fromJS, Map} = require('immutable');
+const {getHttpsApp} = require("../utils");
 const assert = require('assert');
-const https = require("https");
-const connect = require("connect");
 const serverAssert = require("../utils").serverAssert;
-const {init, Methods} = require('../../');
-const {createWithOptions} = require('../../dist/Browsersync.init');
 
-it.only('can proxy https site', function (done) {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-    const app = connect();
+it('can proxy https site', function (done) {
 
-    const server = https.createServer(app);
-    server.listen();
-
-    const url = `https://localhost:${server.address().port}`;
+    const {app, server, url} = getHttpsApp();
 
     app.use('/', function(req, res) {
-        // language=HTML
-        res.end(`<!doctype html>
-<html lang="en">
-<head>
-    <title>Document</title>
-</head>
-<body>
-    <h1><a href="${url}/about">About</a></h1>
-</body>
-</html>`)
+        res.end(`<h1><a href="${url}/about">About</a></h1>`);
     });
 
     const asserts = (resp, options) => {
         const bsPort = options.getIn(['server', 'port']);
-        assert.equal(resp.text.indexOf(`<h1><a href="//127.0.0.1:${bsPort}/about">About</a></h1>`) > -1, true)
-    }
+        const expectedUrl = `//127.0.0.1:${bsPort}`;
+        const expected = `<h1><a href="${expectedUrl}/about">About</a></h1>`;
+        assert.equal(resp.text, expected);
+    };
 
     serverAssert({proxy: [url], scheme: 'https'}, '/', asserts)
         .do(() => server.close())
-        .subscribe(() => {
-            done();
-        }, err => done(err));
+        .subscribe(() => done(), err => done(err));
+});
+
+it('can proxy https site with multiple proxies', function (done) {
+
+    const {app, server, url} = getHttpsApp();
+
+    app.use('/', function(req, res) {
+        res.end([
+            '<h1>',
+            `<a href="${url}/about">About</a>`,
+            `<a href="https://example.com">Back</a>`,
+            '</h1>'
+        ].join(''));
+    });
+
+    const asserts = (resp, options) => {
+        const bsPort = options.getIn(['server', 'port']);
+        const expectedUrl = `//127.0.0.1:${bsPort}`;
+        const expected = [
+            '<h1>',
+                `<a href="${expectedUrl}/about">About</a>`,
+                `<a href="${expectedUrl}">Back</a>`,
+            '</h1>'
+        ].join('');
+
+        assert.equal(resp.text, expected);
+    };
+
+    serverAssert({proxy: [url, 'https://example.com'], scheme: 'https'}, '/', asserts)
+        .do(() => server.close())
+        .subscribe(() => done(), err => done(err));
+});
+
+it('can proxy https sites with mixed schemes', function (done) {
+
+    const {app, server, url} = getHttpsApp();
+
+    app.use('/', function(req, res) {
+        res.end([
+            '<h1>',
+            `<a href="${url}/about">About</a>`,
+            `<a href="https://example.com">Secure</a>`,
+            `<a href="http://example.com">Insecure</a>`,
+            '</h1>'
+        ].join(''));
+    });
+
+    const asserts = (resp, options) => {
+        const bsPort = options.getIn(['server', 'port']);
+        const expectedUrl = `//127.0.0.1:${bsPort}`;
+        const expected = [
+            '<h1>',
+                `<a href="${expectedUrl}/about">About</a>`,
+                `<a href="${expectedUrl}">Secure</a>`,
+                `<a href="${expectedUrl}">Insecure</a>`,
+            '</h1>'
+        ].join('');
+
+        assert.equal(resp.text, expected);
+    };
+
+    serverAssert({
+        proxy: [url, 'https://example.com', 'http://example.com'],
+        scheme: 'https'
+    }, '/', asserts)
+        .do(() => server.close())
+        .subscribe(() => done(), err => done(err));
 });
--- test/utils.js
+++ test/utils.js
@@ -3,8 +3,11 @@ const {Methods} = require('../dist');
 const assert = require('assert');
 const request = require('supertest-as-promised');
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
+
 module.exports.serverAssert = function serverAssert(options, path, assertions) {
     const {bs, server} = require('../dist').create();
+    // console.log(options);
     return bs.ask(Methods.Init, options)
         .flatMap(([errors, output]) => {
             if (errors && errors.length) {
@@ -17,7 +20,26 @@ module.exports.serverAssert = function serverAssert(options, path, assertions) {
             )
                 .map((resp) => [resp, output.options])
         })
-        .catch(err => Observable.concat(bs.ask(Methods.Stop), Observable.throw(err)))
         .do(([resp, options]) => assertions(resp, options))
+        .catch(err => Observable.concat(bs.ask(Methods.Stop), Observable.throw(err)))
         .flatMap(() => bs.ask(Methods.Stop))
+}
+
+
+module.exports.getHttpsApp = function() {
+    const https = require("https");
+    const connect = require("connect");
+    const {readFileSync} = require('fs');
+
+    const app = connect();
+    const httpsOptions = {
+        key: readFileSync('server/certs/server.key'),
+        cert: readFileSync('server/certs/server.crt')
+    };
+
+    const server = https.createServer(httpsOptions, app);
+    server.listen();
+
+    const url = `https://localhost:${server.address().port}`;
+    return {app, url, server};
 }
