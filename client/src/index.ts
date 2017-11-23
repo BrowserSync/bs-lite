2c80613afb146e14c10e9c2f5991ddc752c35d33
Shane Osbourne <shane.osbourne8@gmail.com>
2017-11-23 15:48:01 +0000

    Add nanologger for the client

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,15 +1,16 @@
 import socket = require('socket.io-client');
+const nanlogger = require('nanologger');
+const log = nanlogger('Browsersync', {colors: {magenta: '#0F2634'}});
 
 const {socketConfig, socketUrl, browserSyncOptions} = window.___browserSync___;
 
 const io = socket(socketUrl, socketConfig);
 
 io.on('connection', function(data) {
-    console.log(data);
+    log.info('Connected!', data);
 });
 
 io.on('Browser:Reload', (event) => {
-    console.log('Browser:Reload', event);
-    // window.location.reload(true);
+    log.info('Browser:Reload', event);
 });
 
