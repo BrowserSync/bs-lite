9be30e4ae6416387361f03d9758e6a676aab497e
Shane Osbourne <shane.osbourne8@gmail.com>
2017-11-25 22:05:11 +0000

    implement basics of browser-reload/asset-reload

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,5 +1,6 @@
 import socket = require('socket.io-client');
 import {AssetReload, BrowserMessages, BrowserReload} from "../../src/plugins/Sockets/Clients/BrowserMessageTypes";
+import {FileEvent} from "../../src/plugins/Watcher/FileEvent.message";
 const nanlogger = require('nanologger');
 const log = nanlogger('Browsersync', {colors: {magenta: '#0F2634'}});
 
@@ -13,9 +14,39 @@ io.on('connection', function(data) {
 
 io.on(BrowserMessages.BrowserReload, (message: BrowserReload.Message) => {
     log.info(BrowserMessages.BrowserReload, message);
+    window.location.reload(true);
 });
 
-io.on(BrowserMessages.AssetReload, (message: AssetReload.Message) => {
+io.on(BrowserMessages.AssetReload, (message: AssetReload.Payload) => {
     log.info(BrowserMessages.AssetReload, message);
+    const elems = Array.from(document.getElementsByTagName('LINK'));
+    const names = elems.map((e: any, i) => {
+        const a: any = document.createElement('A');
+        a.href = e.href;
+        const segs = a.pathname.split('/').filter(Boolean);
+        const name = segs.slice(-1)[0];
+        return {
+            name,
+            href: a.href,
+            index: i,
+        };
+    });
+    message.items.forEach((item: FileEvent.Input) => {
+        const matches = names.filter(x => x.name === item.parsed.base);
+        if (matches.length) {
+            log.info(`${matches.length} asset matches found`);
+            matches.forEach(match => {
+                changeCSS(match.href, match.index);
+            });
+        }
+    })
 });
 
+function changeCSS(cssFile, cssLinkIndex) {
+    var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", cssFile);
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+}
