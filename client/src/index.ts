914d20b1617a90a5c89849de97e140fdd46d6f41
Shane Osbourne <shane.osbourne8@gmail.com>
2017-11-29 16:31:23 +0000

    Adding reloader

--- client/src/index.ts
+++ client/src/index.ts
@@ -1,24 +1,34 @@
 import socket = require('socket.io-client');
 import {AssetReload, BrowserMessages, BrowserReload} from "../../src/plugins/Sockets/Clients/BrowserMessageTypes";
-import {FileEvent} from "../../src/plugins/Watcher/FileEvent.message";
-import {replace} from "./assets";
+import Reasons = AssetReload.Reasons;
+import {Reloader} from './vendor/ReReloader';
+import {Timer} from "./vendor/Timer";
 const nanlogger = require('nanologger');
 const log = nanlogger('Browsersync', {colors: {magenta: '#0F2634'}});
+
+const reloader = new Reloader(window, window.console, Timer);
 
 const {socketConfig, socketUrl, browserSyncOptions} = window.___browserSync___;
 
 const io = socket(socketUrl, socketConfig);
 
+log.trace('adding connection listener');
 io.on('connection', function(data) {
     log.info('Connected!', data);
 });
 
+log.trace('adding BrowserMessages.BrowserReload listener');
 io.on(BrowserMessages.BrowserReload, (message: BrowserReload.Message) => {
-    log.info(BrowserMessages.BrowserReload, message);
+    log.debug(BrowserMessages.BrowserReload, message);
     window.location.reload(true);
 });
 
+log.trace('adding BrowserMessages.AssetReload listener');
 io.on(BrowserMessages.AssetReload, (message: AssetReload.Payload) => {
-    log.info(BrowserMessages.AssetReload, message);
-    replace(message, log);
+    log.debug(BrowserMessages.AssetReload, message);
+    if (message.reason === Reasons.FileChanged) {
+        message.items.forEach(item => {
+            reloader.reload(item.path, {liveCSS: true});
+        })
+    }
 });
---
+++ client/src/types.ts
@@ -0,0 +1,8 @@
+export interface Logger {
+    trace(...args: any[]): void
+    debug(...args: any[]): void
+    info(...args: any[]): void
+    warn(...args: any[]): void
+    error(...args: any[]): void
+    fatal(...args: any[]): void
+}
